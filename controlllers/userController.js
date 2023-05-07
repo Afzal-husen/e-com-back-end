0a4254ff8e55fbf4d73bea3d37965d776a5df7d1
Afzal Diwan <afzalhusen3@gmail.com>
2023-05-07 11:53:12 +0530

    updating

--- controlllers/userController.js
+++ controlllers/userController.js
@@ -46,11 +46,10 @@ export const logout = async (req, res, next) => {
       expiration: Date.now(),
       httpOnly: true
     }
-    // res.status(200).cookie("token", null, option).json({
-    //   success: true,
-    //   message: "user Logged out"
-    // })
-    res.clearCookie()
+    res.status(200).cookie("token", null, option).json({
+      success: true,
+      message: "user Logged out"
+    })
   } catch (error) {
     next(error);
   }
